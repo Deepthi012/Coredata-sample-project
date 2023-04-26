1f048b3873930a9ffac7d9083b7dfc36c40f2d67
Deepthi <deepthi.s@westagilelabs.com>
2023-04-26 19:27:29 +0530

    Initial Commit

---
+++ CoreDataTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by West Agile Labs on 26/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
