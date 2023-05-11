96e4cedf6ed6ff9f40c0549647805b85b9ef3e14
Deepthi012 <130980899+Deepthi012@users.noreply.github.com>
2023-05-11 16:57:56 +0530

    Todolist using Coredata

---
+++ CoreDataExample/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  CoreDataExample
+//
+//  Created by West Agile Labs on 02/05/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
